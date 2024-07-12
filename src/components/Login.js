eda1cffd7f72116a2d2c711a9b2a343f295a2ed1
alexandraGoloskok1990 <alexandra.goloskok_new@anstrex.com>
2024-07-12 22:56:55 +0300

    14. Change on control fields of Form and refactoring handle functions

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,41 @@
+import {useState} from "react";
+
 function Login() {
+    const [data, setData] = useState({username: '', password: ''})
+
     const handleOnSubmit = (event) => {
         event.preventDefault()
-        console.log(event.target.password.value)
+        const userData = {username: data.username, password: data.password}
+        console.log(userData)
     }
+
+    const handleInputForm = (e, field) => {
+        setData({ ...data, [field]: e.target.value })
+    }
+
     return (
-        <form onSubmit={handleOnSubmit}>
-            <label><span>Login</span>
-                <input name="login" type="text" />
-            </label>
-            <label><span>Password</span>
-                <input name="password" type="password" />
-            </label>
-            <button type="submit">Submit</button>
-        </form>
+        <div className="loginForm">
+            <h1>Login Form</h1>
+            <form onSubmit={handleOnSubmit}>
+                <label><span>Login</span>
+                    <input
+                        name="login"
+                        value={data.username}
+                        type="text"
+                        onChange={(e) => handleInputForm(e, 'username')}
+                    />
+                </label>
+                <label><span>Password</span>
+                    <input
+                        name="password"
+                        value={data.password}
+                        type="password"
+                        onChange={(e) => handleInputForm(e, 'password')}
+                    />
+                </label>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
     )
 }
 
