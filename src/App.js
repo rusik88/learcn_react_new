4457f8a9c8a506ebc24d95270d29c0df47a8b191
alexandraGoloskok1990 <alexandra.goloskok_new@anstrex.com>
2024-07-13 23:05:31 +0300

    17. Show posts from placeholder

--- src/App.js
+++ src/App.js
@@ -1,37 +1,10 @@
-import './App.css';
-import {useEffect, useState} from "react";
-
+import './App.css'
+import Posts from "./components/Posts";
 
 function App() {
-    const [todo, setTodo] = useState(null)
-    const [status, setStatus] = useState(false)
-
-    useEffect(() => {
-        if(status !== false) {
-            fetch('https://jsonplaceholder.typicode.com/todos/1')
-                .then((res) => res.json())
-                .then((data) => {
-                    setTodo(data)
-                })
-        }
-    }, [status])
-
-    setTimeout(() => {
-        if(status === false) {
-            setStatus(true)
-        }
-    }, 2000)
-
-    if(todo !== null) {
-        console.log(todo)
-    }
-
-
     return (
         <div className="App">
-            {todo !== null &&
-                (<h1>{todo.title}</h1>)
-            }
+            <Posts />
         </div>
     );
 }
