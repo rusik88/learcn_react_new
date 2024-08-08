27cd763d3dd085c7252ff16b14616ee14703ad13
alexandraGoloskok1990 <alexandra.goloskok_new@anstrex.com>
2024-08-08 15:29:40 +0300

    Tests. Install new React project to learn tests

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import BookFilter from "./components/BookFilter/BookFilter";
-import BookList from "./components/BookList/BookList";
-import BookForm from "./components/BookForm/BookForm";
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="app">
-        <header className="app-header">
-            <h1>Library book app</h1>
-        </header>
-        <main className="app-main">
-            <div className="app-left-column">
-                <BookForm />
-            </div>
-            <div className="app-right-column">
-                <BookFilter />
-                <BookList />
-            </div>
-        </main>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- src/redux/books/actionTypes.js
+++
@@ -1,4 +0,0 @@
-export const ADD_TYPE = 'ADD_TYPE'
-export const DEL_TYPE = 'DEL_TYPE'
-export const RANDOM_TYPE = 'RANDOM_TYPE'
-export const FAVOURITE_TYPE = 'FAVOURITE_TYPE'
